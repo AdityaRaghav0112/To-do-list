11b973f9dc9aef2b5882ae65a2af2d813a659c99
AdityaRaghav0112 <adityaraghav239@gmail.com>
2025-11-19 22:11:21 +0530

    installed react-router-dom and fixing a gigantic bug

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
-  
-
   return (
-    <>
+    <Router>
       <Header/>
-    </>
-  )
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,9 @@
-
+import React from 'react'
 
 const Header = () => {
-    const data = [
-        {id: 1, name: '1'},
-        {id: 2, name: '2'},
-        {id: 3, name: '3'},
-        {id: 4, name: '4'}
-    ]
-
   return (
-    <div >
-      This is Header 
-      {data.map((link) => (
-        <a key={link.id}>{link.name}</a>
-      ))}
+    <div>
+      <h1 className='text-3xl'>This is Header</h1>
     </div>
   )
 }
